06041d5eb217d8c03182f3a4175c08e5c9e3119a
emeraldink <ft257746@hotmail.com>
2012-08-28 23:43:35 +1000

    page modifity

--- s1.php
+++ s1.php
@@ -35,19 +35,19 @@ mysql_close($dbcon);
 <br />
 <br />
 <br />
-
+<li>
 <span>Please Enter wine name:</span>
 <input type = "text" name = "winename" id = "winename" />
 <br />
 <br />
 <br />
-<br />
-
-<span>Enter winery name:</span>
+</li>
+<li><span>Enter winery name:</span>
 <input type = "text" name = "wineryname" id = "wineryname" />
 <br />
 <br />
-
+<br />
+</li>
 
 
 <?php
@@ -67,86 +67,109 @@ mysql_select_db("winestore", $dbcon);
 
 $query_region = "select region_name from region ";
 $result_region = mysql_query($query_region, $dbcon);
-echo "Select a region ";
+echo "<li>Select a region ";
 echo "<select name = 'region' id  = 'region'>";
 while ($option = mysql_fetch_row($result_region)){
 	for($j = 0; $j < mysql_num_fields($result_region); $j++){
-		echo "<option value = '$option[$i]'>$option[$i]</option>";
+		echo "<option value = '$option[$j]'>$option[$j]</option>";
 	}
 }
-echo "</select>";
-echo "<br />";
-echo "<br />";
-echo "<br />";
 
 
-$query_grape = "select region_name from grape ";
+?>
+</li>
+</select>
+<br />
+<br />
+<br />
+
+
+<li>Select a grape
+<select name = 'grape' id  = 'grape'>
+<?php
+     
+
+$query_grape = "select variety from grape_variety";
+
 $result_grape = mysql_query($query_grape, $dbcon);
-echo "Select a grape variety";
-echo "<select name = 'grape' id  = 'grape'>";
+
 while ($option = mysql_fetch_row($result_grape)){
-	for($j = 0; $k < mysql_num_fields($result_grape); $k++){
-		echo "<option value = '$option[$i]'>$option[$i]</option>";
+	for($k = 0; $k < mysql_num_fields($result_grape); $k++){
+		echo "<option value = '$option[$k]'>$option[$k]</option>";
 	}
 }
-echo "</select>";
-echo "<br />";
-echo "<br />";
-echo "<br />";
 
-echo "Select years";
-echo "<select name = 'years' id  = 'years'>";
+?>
+</li>
+</select>
+<br />
+<br />
+<br />
 
 
-$query_years = "select region_name from grape ";
+
+
+
+
+<li>Please Select years
+<select name = 'years' id  = 'years'>
+
+<?php
+$query_years = "select distinct year from wine order by year asc;";
 $result_years = mysql_query($query_years, $dbcon);
 while ($option = mysql_fetch_row($result_years)){
-	$year[$i] = mysql_result($result_years, $i);
-
-
+	$years[$i] = mysql_result($result_years, $i);
 
 	for($i = 0; $i <count($year); $i++){
 		
 		//echo "<option value = '$option[$i]'>$option[$i]</option>";
-		echo "<option value = '$year[$i]~$year[$count]'>$year[$i]~$year[$count]</option>";
+		echo "<option value = '$years[$i]~$years[$count]'>$years[$i]~$years[$count]</option>";
 	}
 
 	
 }
-echo "</select>";
-echo "<br />";
-echo "<br />";
-echo "<br />";
-
-
-
-
 ?>
+</li>
+</select>
+<br />
+<br />
+<br />
 
-<span>Minimum number of wines in stock</span>
+
+
+
+<li><span>Minimum number of wines in stock</span>
 <input type = "text" name = "minstock" id = "minstock" value = 0 />
 <br />
 <br />
 <br />
+</li>
+<li>
 <span>Minimum number of wines ordered</span>
 <input type = "text" name = "minorder" id = "minorder" value = 0 />
 <br />
 <br />
 <br />
+</li>
+<li>
 <span>Enter minimum cost:</span>
 <input type = "text" name = "mincost" id ="mincost" value = 0 />
 <br />
 <br />
 <br />
+</li>
+<li>
 <span>Enter maximum cost:</span>
 <input type= "text" name = "maxcost" id="maxcost" value = 0 />
 <br />
 <br />
 <br />
+</li>
 
 <input type = "submit" name = "submit" id = "submit" value = "Search" />
 
 
+<br />
 <br />
 <br />
 
