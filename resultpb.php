66b1e872cd4f7deb2266c589b61b162d46e03a1e
emeraldink <ft257746@hotmail.com>
2012-08-28 21:33:34 +1000

    page modifity

--- resultpb.php
+++ resultpb.php
@@ -10,12 +10,34 @@
 <?php
 	require_once('db.php');
 	if(!mysql_connect(DB_HOST, DB_USER, DB_PW)){
-	echo "Can't connect to database";
+	echo "unable connect to database";
 	}
 	else{
 	$dbcon = mysql_connect(DB_HOST, DB_USER, DB_PW);
 	mysql_select_db('winestore', $dbcon);
 	}
+	
+	
+	
+	function display($dbcon, $query){
+	$result_page = mysql_query($query, $connection);
+	$rowfound = mysql_num_rows($result_page);
+	echo $rowfound." results found";
+	echo "<table width='90%'>";
+	echo "<tr><th align='left'>Name</th>
+      	<th align='left'>Variety</th>
+	  	<th align='left'>Year</th>
+	  	<th align='left'>Winery</th>
+	  	<th align='left'>Region</th>
+	  	<th align='left'>Cost</th>
+	  	<th align='left'>Stock</th>
+	  	<th align='left'>Price</th>
+	  	<th align='left'>Quantity</th>
+	  	<th align='left'>Revenue</th></tr>";
+
+	
+	
+	
 	
 ?>
 
