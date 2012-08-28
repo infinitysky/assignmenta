6e441b722db9bd2a197f93bc2cc77b9a18ed3a8a
emeraldink <ft257746@hotmail.com>
2012-08-28 19:22:33 +1000

    database test

--- s1.php
+++ s1.php
@@ -10,15 +10,44 @@
 <body bgcolor = "white">
 <form action = "resultpb.php" method = "GET">
 
+
+<?php
+require_once('db.php');
+mysql_connect(DB_HOST, DB_USER, DB_PW);
+$dbcon = mysql_connect(DB_HOST, DB_USER, DB_PW);
+
+if(!$dbcon)
+{
+//echo 'Could not connect to mysql on ' . DB_HOST . "\n";
+//exit;
+die('Can not accesse '.mysqlerror());
+}
+echo 'Connected to mysql on ' . DB_HOST . "\n";
+mysql_select_db("winestore", $dbcon);
+
+
+$query = "select region_name from region ";
+$result = mysql_query($query, $dbcon);
+?>
+
+
+
+
+
+<br />
+<br />
+<br />
 <span>Please Enter wine name:</span>
 <input type = "text" name = "winename" id = "winename" value = "All"/>
 <br />
 <br />
-
+<br />
+<br />
 <span>Enter winery name:</span>
 <input type = "text" name = "wineryname" id = "wineryname" />
 <br />
 <br />
+<input type = "submit" name = "submit" id = "submit" value = "Search" />
 
 </form>
 </body>
