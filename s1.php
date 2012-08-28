b6c558c701b224a6275f185556ba3d922a8a08d2
emeraldink <ft257746@hotmail.com>
2012-08-28 11:38:04 +1000

    searchpage

---
+++ s1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<title>Search Page</title>
+</head>
+
+<body bgcolor = "white">
+<form action = "resultpb.php" method = "GET">
+
+<span>Please Enter wine name:</span>
+<input type = "text" name = "winename" id = "winename" value = "All"/>
+<br />
+<br />
+
+<span>Enter winery name:</span>
+<input type = "text" name = "wineryname" id = "wineryname" />
+<br />
+<br />
+
+</form>
+</body>
